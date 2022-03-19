b5bbc66b760340f1e4de0dbb9f82cfb4c797ce77
Kaspersyv <kaspersyv@viken.no>
2022-03-19 17:39:16 +0100

    Attempt at fixing bug

--- docs/script.js
+++ docs/script.js
@@ -5,6 +5,7 @@ let userList = localStorage.getItem("userList");
 
 userInput.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
+        console.log("Enter!")
         addEntry();
     }
 });
