dcb0478cf95c189faf2c601872099ec9b0b8fe15
Kaspersyv <kaspersyv@viken.no>
2022-03-19 11:35:35 +0100

    Some updates

--- docs/script.js
+++ docs/script.js
@@ -1,38 +1,37 @@
+const userInput = document.getElementById("userInput")
+const finalInput = JSON.stringify(userInput)
+
+let userField = document.getElementById("userInput");
+
+if (sessionStorage.getItem("autosave")) {
+    userField.value = sessionStorage.getItem("autosave");
+}
+
+userField.addEventListener("change", function(){
+    sessionStorage.setItem("autosave", userField.value);
+    console.log(sessionStorage.getItem("autosave"));
+})
 
 
 
-   let userInput = document.getElementById("userInput")
-   let finalInput = JSON.stringify(userInput)
+let current = new Date();
 
-  function welcome() {
-      sessionStorage.setItem("name", finalInput)
-      const userName = sessionStorage.getItem("name")
-      $("#nameField").innerText = "Velkommen " + userName;
-  }
+let firstRandom = Math.floor(Math.random() * 8) + 1;
 
+let lastMinute = new Date().getMinutes();
 
-  const userName = sessionStorage.getItem("name")
+const checkMinute = () => {
+    let randomNumber = Math.floor(Math.random() * 8) + 1;
+    thisMinute = new Date().getMinutes();
+    if (thisMinute !== lastMinute) {
+        lastMinute = thisMinute
+        document.getElementById("clockField").innerText = "Ditt lykketall er: " + randomNumber
+    }
 
-  let current = new Date();
-
-  let firstRandom = Math.floor(Math.random() * 8) + 1;
-
-  let lastMinute = new Date().getMinutes();
-
-  const checkMinute = () => {
-      let randomNumber = Math.floor(Math.random() * 8) + 1;
-      thisMinute = new Date().getMinutes();
-      if (thisMinute !== lastMinute) {
-          lastMinute = thisMinute
-          document.querySelector("#clockField").innerText = "Ditt lykketall er: " + randomNumber
-      }
-
-  }
+}
 
 
 
 
-  window.setInterval(checkMinute, 1000);
-  document.getElementById("clockField").innerText = "Your lucky number is: " + firstRandom
-
-
+window.setInterval(checkMinute, 1000);
+document.getElementById("clockField").innerText = "Your lucky number is: " + firstRandom
